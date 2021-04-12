d8b52f3ba087117a8ec2772d489a817a780e5c86
MathiasEriksen <59641976+MathiasEriksen@users.noreply.github.com>
2021-04-12 12:18:03 +0200

    Add files via upload

---
+++ LogicMediaOpgave/LogicMediaOpgave/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LogicMediaOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liste af 100"); // Du har en liste af tal fra 1 - 100. 
+            for (int i = 1; i < 101; i++) //Jeg vil gerne du løber igennem listen. 
+            {
+                if (i % 3 == 0 & i % 5 == 0) // For hvert tal der kan deles ligeligt med 3 OG 5 skal du udskrive “[Fornavn][Efternavn]”. 
+                {
+                    Console.WriteLine("Mathias Munch" + ","); 
+                }
+                else if (i % 3 == 0) // For hvert tal i listen der kan deles ligeligt med 3 skal du udskrive dit [Fornavn]. 
+                {
+                    Console.WriteLine("Mathias" + ","); 
+                }
+                else if (i % 5 == 0) //For hvert tal der kan deles ligeligt med 5 skal du udskrive dit [Efternavn]. 
+                {
+                    Console.WriteLine("Munch" + ","); 
+                }
+                else
+                {
+                    Console.WriteLine(i + ",");
+                }
+
+            }
+        }
+    }
+}
